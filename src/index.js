cb4b7021a7bb02aee551fe19d276f889601edc92
Lloyd Akresi <70116121+lloydakresi@users.noreply.github.com>
2023-11-22 11:13:16 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { HoroscopeContext } from './context/HoroscopeContext';
 import App from './App';
 
 const Root = () => {
   return (
+    <HoroscopeContext.Provider value={{sign: "Leo"}}>
       <App />
+    </HoroscopeContext.Provider>
   );
 };
 
